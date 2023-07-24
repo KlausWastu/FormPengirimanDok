6f852bf3ee2eb4abec1201d771684e630b6405e3
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-24 08:58:47 +0700

    Salin ke Server

---
+++ db/index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const { urlDb } = require("../config");
+
+mongoose.connect(urlDb, {
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+module.exports = db;
