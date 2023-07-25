b7e009d831c7a0e58613fe26d9ca518acd0ee5d2
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-25 09:47:21 +0700

    Membuat controller untuk menampilkan dan logika penyimpanan data formulir kedalam database

--- app/formulir/controller.js
+++ app/formulir/controller.js
@@ -1,9 +1,55 @@
+const Form = require("./model");
 module.exports = {
-  index: (req, res) => {
+  index: async (req, res) => {
     try {
-      res.render("index", {
+      const form = await Form.find({ isdeleted: false });
+      res.render("admin/formPengiriman/view_form", {
         title: "Formulir Pengiriman",
+        form,
       });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/formPengiriman/create", {
+        title: "Tambah Form",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const {
+        pembuat,
+        kepada,
+        bagian,
+        tanggal,
+        kebijakan,
+        rencana,
+        prosedur,
+        sasaran,
+        pedoman,
+        dokpendukung,
+      } = req.body;
+      const jenisDok = [
+        kebijakan,
+        rencana,
+        prosedur,
+        sasaran,
+        pedoman,
+        dokpendukung,
+      ].filter(Boolean);
+      const form = new Form({
+        dibuat: pembuat,
+        kepada,
+        bagian,
+        tanggal,
+        jenisdok: jenisDok,
+      });
+      await form.save();
     } catch (err) {
       console.log(err);
     }
