44debaa859ae489c051a81a5366890268a3d5b91
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-27 11:27:13 +0700

    Update halaman pengguna

--- app/pengguna/controller.js
+++ app/pengguna/controller.js
@@ -8,10 +8,12 @@ module.exports = {
       const alertStatus = req.flash("alertStatus");
 
       const alert = { message: alertMessage, status: alertStatus };
+      const user = await Pengguna.find({ role: ["user", "sekdir", "dokon"] });
       res.render("admin/adduser/viewPengguna", {
         title: "Pengguna",
         name: req.session.user.name,
         alert,
+        user,
       });
     } catch (err) {
       req.flash("alerMessage", `${err.message}`);
