334272efac4087620ee43867bd48c231df38ea8b
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-27 10:47:58 +0700

    Menambahkan logika popup message dan memperbaiki call untuk error

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,11 +1,19 @@
 module.exports = {
   index: async (req, res) => {
     try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
       res.render("admin/dashboard/view_dashboard", {
         title: "Dashboard",
+        name: req.session.user.name,
+        alert,
       });
     } catch (err) {
-      console.log(err);
+      req.flash("alerMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/");
     }
   },
 };
