d2a65ab854d0e66d6445d5a98c4a1ebf2bc80255
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-27 10:49:10 +0700

    Menambahkan router untuk lihat dokumen pada formulir

--- app/formulir/router.js
+++ app/formulir/router.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
-const { index, viewCreate, actionCreate } = require("./controller");
+const { index, viewCreate, actionCreate, viewDok } = require("./controller");
+const { isLogin } = require("../middleware/auth");
 /* GET home page. */
+router.use(isLogin);
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", actionCreate);
+router.get("/reviewDok/:id", viewDok);
 
 module.exports = router;
