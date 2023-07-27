2611d9f7f1cea0f730df2047af8791939cdbbdfd
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-27 10:48:30 +0700

    Menambahkan router middleware

--- app/dashboard/router.js
+++ app/dashboard/router.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const { index, viewCreate } = require("./controller");
+const { isLogin } = require("../middleware/auth");
 /* GET home page. */
+router.use(isLogin);
 router.get("/", index);
 // router.get("/create", viewCreate);
 
