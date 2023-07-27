9bb28b29cf901c71043bf09b92f2cf775aaafd1a
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-27 15:40:17 +0700

    Membuat logika untuk menambahkan pengguna dan menyimpan dalam database

--- app/pengguna/controller.js
+++ app/pengguna/controller.js
@@ -21,4 +21,63 @@ module.exports = {
       res.redirect("/");
     }
   },
+  viewtambahPengguna: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
+      res.render("admin/adduser/create", {
+        title: "Tambah Pengguna",
+        name: req.session.user.name,
+        alert,
+      });
+    } catch (err) {
+      console.log(err);
+      req.flash("alerMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/");
+    }
+  },
+  tambahPengguna: async (req, res) => {
+    try {
+      const { email, password, nama, jabatan, role } = req.body;
+      const user = await Pengguna.findOne({ email });
+      console.log("User >> ", user);
+      if (user) {
+        req.flash("alertMessage", "Email sudah ada");
+        req.flash("alertStatus", "success");
+        res.redirect("/");
+      } else {
+        let hashpass = await bcrypt.hash(password, 10);
+        const pengguna = await Pengguna({
+          email,
+          password: hashpass,
+          name: nama,
+          jabatan,
+          role,
+        });
+        let Role;
+        if (role === "user") {
+          Role = "User";
+        } else if (role === "sekdir") {
+          Role = "Sekertaris Direksi";
+        } else if (role === "dokon") {
+          Role = "Dokumen Kontrol";
+        }
+        await pengguna.save();
+        req.flash(
+          "alertMessage",
+          `Berhasil menambahkan ${nama} menjadi ${Role}`
+        );
+        req.flash("alertStatus", "success");
+        res.redirect("/pengguna");
+      }
+    } catch (err) {
+      console.log(err);
+      req.flash("alerMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/");
+    }
+  },
 };
