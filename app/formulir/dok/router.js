f4f9dfbd252db97e93730e7d63331c4f25fbbd53
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-28 09:38:24 +0700

    Update router  dokumen

--- app/formulir/dok/router.js
+++ app/formulir/dok/router.js
@@ -1,11 +1,20 @@
 var express = require("express");
 var router = express.Router();
 const { index, viewCreate, actionCreate } = require("./controller");
+const multer = require("multer");
+const os = require("os");
+
 const { isLogin } = require("../../middleware/auth");
 /* GET home page. */
 router.use(isLogin);
 router.get("/", index);
 router.get("/create", viewCreate);
-router.post("/create", actionCreate);
+router.post(
+  "/create",
+  multer({ dest: os.tmpdir(), limits: { fileSize: 2097152 } }).single(
+    "uploaddokumen"
+  ),
+  actionCreate
+);
 
 module.exports = router;
