1af2f06ecc7bb9d4279cf7ace08114bdf33d6e98
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-31 11:14:12 +0700

    Menambahkan router untuk ubahpassword

--- app/user/router.js
+++ app/user/router.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
-const { viewSignin, actionSignin, logout } = require("./controller");
+const {
+  viewSignin,
+  actionSignin,
+  logout,
+  viewUbahPass,
+  actionUbahPass,
+} = require("./controller");
 /* GET home page. */
 router.get("/", viewSignin);
 router.post("/", actionSignin);
+router.get("/ubahpassword", viewUbahPass);
+router.put("/ubah/:id", actionUbahPass);
 router.get("/logout", logout);
 
 module.exports = router;
