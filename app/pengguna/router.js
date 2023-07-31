e3aefa19d68b22a646f9d0fb8c0ba8e217cd5865
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-31 11:25:53 +0700

    Menambahkan router untuk update status

--- app/pengguna/router.js
+++ app/pengguna/router.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var router = express.Router();
-const { index, viewtambahPengguna, tambahPengguna } = require("./controller");
+const {
+  index,
+  viewtambahPengguna,
+  tambahPengguna,
+  status,
+} = require("./controller");
 const { isLogin } = require("../middleware/auth");
 /* GET home page. */
 router.use(isLogin);
 router.get("/", index);
 router.get("/create", viewtambahPengguna);
 router.post("/create", tambahPengguna);
-// router.get("/create", viewCreate);
-// router.post("/create", actionCreate);
+router.put("/status/:id", status);
 
 module.exports = router;
